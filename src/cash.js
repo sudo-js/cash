211e68f1d110c20642ef7bdc63af62c21ed940c1
robrobbins <rob.robb.ns@gmail.com>
2014-03-08 20:21:20 -0800

    that was dumb

--- src/cash.js
+++ src/cash.js
@@ -289,7 +289,7 @@
           // base case is that this is not 'delegated'
           if(!sel) fn.call(el, e);
           // there is a sel, check for matches and call if so.
-          else if(~$(el).find(sel).q.indexOf(e.target) || (targ = $(el).find(sel).contains(e.target).q[0])) {
+          else if(~$(el).find(sel).q.indexOf(e.target) || (targ = $.contains(e.target).q[0])) {
             targ || (targ = e.target);
             e.currentTarget = targ;
             fn.call(targ, e);
